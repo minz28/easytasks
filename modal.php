81919ef31dbf6896b6a868acda6b4edc8cd5c969
minz28 <mike_funky@live.cl>
2017-05-31 17:22:31 -0500

    Add files via upload

---
+++ modal.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, maximun-scale=1">
+<title>EasyTask | Gestión visual de tareas</title>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/funciones.js"></script>
+
+
+
+</head>
+
+<body>
+<div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <a href="https://www.w3schools.com/bootstrap/bootstrap_modal.asp">Modal Explain 1</a><br>
+  <a href="http://blog.gtoeurope.es/crear-modal-en-bootstrap/">Modal Explain 2</a><br>
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>  
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+
+</body>
+</html>
