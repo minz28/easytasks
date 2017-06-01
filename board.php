b340559baf50f7d41f8fc13555347cf84c42014c
minz28 <mike_funky@live.cl>
2017-06-01 08:09:59 -0500

    Add files via upload

--- board.php
+++ board.php
@@ -19,6 +19,60 @@ $funciones = new Funciones;
 
 <body style="background-color:#EAEAEA;">
 
+<nav class="navbar navbar-default navbar-fixed">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-left">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
 <header>
     <div class="container">
     	<div class="row">
@@ -35,16 +89,15 @@ $funciones = new Funciones;
     </div>
 </header>
 
-<div class="container">
+<div class="container">    
     <div class="row">
-    	<div class="col-md-1">
-    		<a href="creaTarjeta.php" id="popup"><img src="img/site/add.png" style="width: 50px;"></a>
-    		<!--<button id="popup" onclick="div_show()">Popup</button>-->
-    	</div>
+        <div class="col-md-1">
+            <button type="button" class="btn btn-default" data-toggle="modal" data-target="#creaTarjeta"><span class="glyphicon glyphicon-plus"></span></button>
+        </div>
     </div>
     <div class="row">
-    	<div class="col-md-3">
-            <table class="table table-bordered table-hover table-condensed">
+        <div class="col-md-3">
+            <table class="table table-bordered table-hover table-condensed" style="border-radius: 10px;">
                 <tr>
                     <th class="warning text-center">PENDIENTES</th>
                 </tr>
@@ -74,84 +127,67 @@ $funciones = new Funciones;
                 </tr>
                 <?php $funciones->muestraTarjetaImpedidas(); ?>
             </table>
-        </div>            
-    	<?php //$funciones->muestraTablero(); ?>
-        <!--<div class="col-md-12">
-        	<table class="table table-bordered table-hover table-condensed">
-    			<tr>
-    				<th class="success">Pendiente</th>
-    				<th class="warning">En desarrollo</th>
-    				<th class="danger">Terminada</th>
-    				<th class="info">Impedida</th>
-    			</tr>
-    			<?php $funciones->muestraTablero();?>
-    			<tr>
-    				<td class="success">a</td>
-    				<td class="warning">b</td>
-    				<td class="danger">c</td>
-    				<td class="info">d</td>
-    			</tr>    			
-    		</table>
-    	<!--</div>-->
+        </div>
     </div>
+    <!--Inicio modal add tarea-->
+    <div class="modal fade" id="creaTarjeta" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Crear Tarjeta</h4>
+                    <small>(*) Campos obligatorios</small>
+                </div>
+                <div class="modal-body">
+                    <form id="form">
+                        <input type="hidden" name="pagina" value="creaTarjeta" /><!--Variable oculta para identificar en el controlador-->
+                        <div class="form-group">
+                            <label for="cboTarea">Tarea (*)</label>
+                            <select class="form-control" id="cboTarea" name="cboTarea">
+                                <option value="seleccione">Seleccione</option>
+                                <?php $funciones->cboTarea(); ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="txtSolicitante">Solicitante (*)</label>
+                            <select class="form-control" id="txtSolicitante" name="txtSolicitante">
+                                <option value="seleccione">Seleccione</option>
+                                <?php $funciones->cboCliente(); ?>
+                            </select>
+                        </div>              
+                        <div class="form-group">
+                            <label for="cboPrioridad">Prioridad (*)</label>
+                            <select class="form-control" id="cboPrioridad" name="cboPrioridad">
+                                <option value="seleccione">Seleccione</option>
+                                <?php $funciones->cboPrioridad(); ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="cboEstado">Estado (*)</label>
+                            <select class="form-control" id="cboEstado" name="cboEstado">
+                                <option value="seleccione">Seleccione</option>
+                                <?php $funciones->cboEstTarjeta(); ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="txtObservaciones">Observaciones</label>
+                            <textarea class="form-control" id="txtObservaciones" name="txtObservaciones" rows="4"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="fileAdjunto">Archivo adjunto</label>
+                            <input type="file" id="fileAdjunto"  name="fileAdjunto">
+                        </div>                        
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" onclick="validaTarjeta();">Grabar</button>
+                </div>                
+            </div>
+        </div>
+    </div>
+    <!--Fin modal add tarea-->
 </div>
-<!--
-<div class="container" id="formTarea" style="display: block;">
-	<div class="row">
-		<div class="col-md-3">
-			<form>
-				<div class="form-group">
-					<label for="selectTarea">Tarea</label>
-					<select class="form-control" id="selectTarea">
-						<option>Seleccione</option>
-						<option>a</option>
-						<option>b</option>
-						<option>c</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label for="textSolicitante">Solicitante</label>
-					<input type="text" class="form-control" id="textoSolicitante" placeholder="Solicitante">
-				</div>
-				<div class="form-group">
-					<label for="textDias">Días estimados de duración</label>
-					<input type="text" class="form-control" id="textDias" placeholder="Días estimados">
-				</div>
-				<div class="form-group">
-					<label for="textHoras">Horas estimadas de duración</label>
-					<input type="text" class="form-control" id="textoSolicitante" placeholder="Horas estimadas">
-				</div>
-				<div class="form-group">
-					<label for="selectPrioridad">Prioridad</label>
-					<select class="form-control" id="selectPrioridad">
-						<option>Seleccione</option>
-						<option>a</option>
-						<option>b</option>
-						<option>c</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label for="selectEstado">Estado</label>
-					<select class="form-control" id="selectEstado">
-						<option>Seleccione</option>
-						<option>a</option>
-						<option>b</option>
-						<option>c</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label for="textObservaciones">Observaciones</label>
-					<textarea class="form-control" id="textObservaciones" rows="4"></textarea>
-				</div>
-				<div class="form-group">
-					<label for="fileAdjunto">Archivo adjunto</label>
-					<input type="file" id="fileAdjunto">
-				</div>				
-			</form>
-		</div>		
-	</div>
-</div>
--->
+
 <div class="panel-footer navbar-fixed-bottom">
     <div class="container">
         <p>EasyTask® 2017 | Miguel Pinto - Miguel Inzunza - Hans Silva | Todos los derechos reservados</p>
