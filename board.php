7c341b923a2531097d10fb0adda1135ff9d9c1b7
minz28 <mike_funky@live.cl>
2017-06-01 20:53:57 -0400

    Navbar - Menú
    Estructura menú

--- board.php
+++ board.php
@@ -17,25 +17,25 @@ $funciones = new Funciones;
 <script type="text/javascript" src="js/funciones.js"></script>
 </head>
 
-<body style="background-color:#EAEAEA;">
+<body>
 
 <nav class="navbar navbar-default navbar-fixed">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Brand</a>
+	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+        	</button>
+        	<a class="navbar-brand" href="#">EasyTasks</a>
         </div>
-
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
             <li><a href="#">Link</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -49,27 +49,15 @@ $funciones = new Funciones;
                     <li><a href="#">One more separated link</a></li>
                 </ul>
             </li>
-        </ul>
-        <form class="navbar-form navbar-left">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
+        </ul>-->
+        <div class="row">
+        	<div class="col-md-4 col-md-offset-4">           	
+            	<h5 class="text-right">Bienvenido <?php echo $_SESSION['nombreUsuario']; ?></h5>            	
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                </ul>
-            </li>
-        </ul>
-        </div><!-- /.navbar-collapse -->
+            <div class="col-md-2">
+            	<a href="logout.php"><button type="button" class="btn btn-default">Cerrar Sesión</button></a>
+            </div>
+        </div>
     </div><!-- /.container-fluid -->
 </nav>
 
@@ -79,12 +67,12 @@ $funciones = new Funciones;
         	<div class="col-md-4">
         	   <h4>Tablero de tareas</h4>
             </div>
-            <div class="col-md-6">           	
+            <!--<div class="col-md-6">
             	<h5 class="text-right">Bienvenido <?php echo $_SESSION['nombreUsuario']; ?></h5>            	
             </div>
-            <div class="col-mod-2">
+            <div class="col-md-2">
             	<a href="logout.php"><button type="button" class="btn btn-default">Cerrar Sesión</button></a>
-            </div>
+            </div>-->
         </div>        
     </div>
 </header>
